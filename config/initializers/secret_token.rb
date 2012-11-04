d800c9d14346f37ecc1577943f2d91a26a48490f
Athena Avalos <athena.avalos@gmail.com>
2012-11-03 19:59:16 -0600

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '611be7968ca963f112ad50defd097e4656dace6959e431e35cb68039432753716e0e8f7f80d05ae23b71814a28331c62fc3b50bfc9408f8b37b42de0b4a2a62e'
---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+Product.delete_all
+# . . .
+Product.create(title: 'Floral Turquoise Opal Ring',
+  description: 
+    %{<p>
+        This is a special and unique piece that will be sure to grab attention! The Floral Turquoise Opal Ring is hand crafted from genuine sterling silver and beautifully decorated with Turquoise and Opal inlay. Mini silver beads line the inlay and floral designs making for a hallmark finish and a beautiful decoration to any outfit! 
+      </p>},
+  image_url:   '/images/WS218612.jpg',    
+  price: 36.00)
+# . . .
+Product.create(title: 'Cubic Zirconia Pendant And Sterling Silver 18" Italian Chain Necklace',
+  description:
+    %{<p>
+        This luxurious pendant is for anyone who is looking to dress up their evening wear with keeping that Southwest feeling. The Cubic Zirconia Pendant is made out of genuine sterling silver and Cubic Zirconia stones. The pendant has a total of one hundred and eighty-eight Cubic Zirconia stones which accompanies well to the 0.925 sterling silver for which the pendant is stamped. An Italian chain accompanies this pendant and is also sterling silver. The necklace is stamped Italy and 0.925 sterling silver as well. Look stunning in any dress with this fabulous accessory!!
+      </p>},
+  image_url: '/images/WS198109.jpg',
+  price: 49.95)
+# . . .
+Product.create(title: 'Sterling Silver Jewelry Red Oyster Shell Post Dangle Earrings',
+   description:
+     %{<p>
+        Be the talk of the town with these unique Sterling Silver Jewelry Red Oyster Shell Earrings are made from genuine sterling silver and real red oyster shells. They can be worn with both casual wear and formal wear making the perfect accessory to any outfit!
+       </p>},
+   image_url: '/images/WS133008.jpg',
+   price: 42.95)
+# . . .
+
